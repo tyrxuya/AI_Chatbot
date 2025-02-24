af8ec803ba7948bbd4b4da205e00bf172ee2230c
vankata <ivan2003444@gmail.com>
2025-02-24 14:21:41 +0200

    Implemented a password hasher

---
+++ AIChatbot/API/IPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIChatbot.API
+{
+    public interface IPasswordHasher
+    {
+        public string Hash(string password);
+    }
+}
---
+++ AIChatbot/Business/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using AIChatbot.API;
+
+namespace AIChatbot.Business
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(password);
+            byte[] digest = SHA256.HashData(data);
+
+            return Encoding.ASCII.GetString(digest);
+        }
+    }
+}
